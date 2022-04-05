db56a8bb8ac9b1dab6a1e7e2c77601053ffc557c
Sheriff Amzat <kunmiamzat1@gmail.com>
2022-04-05 12:45:44 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import {ReactComponent as DividerDesktop} from './images/pattern-divider-desktop.svg';
+import {ReactComponent as DividerMobile} from './images/pattern-divider-mobile.svg';
+import {ReactComponent as Dice} from "./images/icon-dice.svg";
+import {useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+const [advice, setAdvice] = useState();
+const [id, setId] = useState();
+
+useEffect(() => {
+  getAdvice();
+});
+
+const getAdvice = async() => {
+  await axios("https://api.adviceslip.com/advice")
+  .then(response => {
+    setAdvice(response.data.slip.advice)
+    setId(response.data.slip.id)
+  })
+  .catch((error) => {
+    console.error("Error fetching data: ", error);
+  })
+};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className='advice-div'>
+        <h4>{`ADVICE #${id}`}</h4>
+        <p className='p'>{`"${advice}"`}</p>
+        <DividerDesktop className='desktop'/>
+        <DividerMobile className='mobile'/>
+        </div>
+        <button className='dice-button' onClick={getAdvice}>
+        <Dice className='dice'/>
+        </button>
     </div>
   );
 }
